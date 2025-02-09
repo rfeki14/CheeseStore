28f0b80479d0461b290e36bba408e2fb55789a2b
MedAziz29 <103658249+MedAziz29@users.noreply.github.com>
2025-02-09 10:43:40 +0100

    Update password_reset.php

--- password_reset.php
+++ password_reset.php
@@ -6,39 +6,47 @@
   }
 ?>
 <?php include 'includes/header.php'; ?>
+
+<!-- Link to External CSS -->
+<link rel="stylesheet" href="dist/css/login.css">
+
 <body class="hold-transition login-page">
-<div class="login-box">
-  	<?php
+  <div class="overlay"></div>
+
+  <div class="login-box">
+    <?php
       if(isset($_SESSION['error'])){
-        echo "
-          <div class='callout callout-danger text-center'>
-            <p>".$_SESSION['error']."</p> 
-          </div>
-        ";
+        echo "<div class='callout callout-danger'>".$_SESSION['error']."</div>";
         unset($_SESSION['error']);
       }
+      if(isset($_SESSION['success'])){
+        echo "<div class='callout callout-success'>".$_SESSION['success']."</div>";
+        unset($_SESSION['success']);
+      }
     ?>
-  	<div class="login-box-body">
-    	<p class="login-box-msg">Enter new password</p>
 
-    	<form action="password_new.php?code=<?php echo $_GET['code']; ?>&user=<?php echo $_GET['user']; ?>" method="POST">
-      		<div class="form-group has-feedback">
-        		<input type="password" class="form-control" name="password" placeholder="New password" required>
-        		<span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      		</div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" name="repassword" placeholder="Re-type password" required>
-            <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-          </div>
-      		<div class="row">
-    			<div class="col-xs-4">
-          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="reset"><i class="fa fa-check-square-o"></i> Reset</button>
-        		</div>
-      		</div>
-    	</form>
-  	</div>
-</div>
-	
-<?php include 'includes/scripts.php' ?>
+    <div class="login-box-body">
+      <h2 class="text-primary mb-3">Reset Your Password</h2>
+
+      <form action="password_new.php?code=<?php echo $_GET['code']; ?>&user=<?php echo $_GET['user']; ?>" method="POST">
+        <div class="form-group">
+          <input type="password" class="form-control" name="password" placeholder="New Password" required>
+        </div>
+
+        <div class="form-group">
+          <input type="password" class="form-control" name="repassword" placeholder="Re-type Password" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary mt-3" name="reset"><i class="fa fa-check-square-o"></i> Reset Password</button>
+      </form>
+
+      <div class="login-links">
+        <a href="login.php">Back to Login</a>
+        <a href="index.php"><i class="fa fa-home"></i> Back to Home</a>
+      </div>
+    </div>
+  </div>
+
+<?php include 'includes/scripts.php'; ?>
 </body>
 </html>
