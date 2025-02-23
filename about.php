f9c741cd128d72b1e94ab5cb91d8b7b62f555239
MedAziz29 <103658249+MedAziz29@users.noreply.github.com>
2025-02-23 20:05:50 +0100

    Update about.php
    update aziz about

--- about.php
+++ about.php
@@ -1,13 +1,16 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/navbar.php'; ?>
+
 <link rel="stylesheet" href="dist/css/about.css"> 
-<h1 class="title">À propos</h1>
+
 <div class="about-section">
-    
     <div class="about-container">
         <!-- Left: Image -->
         <div class="about-image">
             <img src="images/about.png" alt="Cœur Blanc Fromagerie">
         </div>
-        <!-- Right: Content --> 
+        <!-- Right: Content -->
         <div class="about-content">
             <h1 class="about-title">Chez Cœur Blanc, la passion du fromage, <br> c'est une histoire de famille.</h1>
             <p>
@@ -15,17 +18,11 @@
                 où l’on trouve les meilleures races de vaches, nourries avec une alimentation saine et soignées avec attention.  
                 En retour, nous obtenons le lait le plus pur et de la plus haute qualité,  
                 reconnu aussi bien par les proches que par les lointains.  
-                <br><br>
-                Avec notre amour et notre passion,  
-                nous fabriquons les meilleurs fromages, avec sincérité et dévouement.  
-                Parce que vous avez une place spéciale dans nos cœurs,  
-                vous, notre précieuse cliente et notre précieux client,  
-                nous vous offrons ce que nous avons de plus cher.  
-                <br><br>
-                Le secret de ce fromage ? Il est fait avec <strong>100 % de pureté et de cœur.</strong>  
-                <br><br>
-                <em>Ce fromage est exempt de tout ce qui est négatif, mais rempli d’amour.</em>  
             </p>
         </div>
     </div>
 </div>
+
+<?php include 'includes/footer.php'; ?>
+</body>
+</html>
